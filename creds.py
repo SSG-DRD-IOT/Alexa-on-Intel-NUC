f86c7d4d7aa6d54eac0d0076412c1af33a175b72
martinkronberg <kronberg.martin@gmail.com>
2016-12-20 11:28:51 -0800

    added creds

--- creds.py
+++ creds.py
@@ -2,11 +2,10 @@
 import os
 
 #Alexa Settings
-ProductID = ""
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
+ProductID = "nuc_alexa01"
+Security_Profile_Description ="Alexa Voice Service App Security Profile Description"
+Security_Profile_ID  = "amzn1.application.c09663029ba24b4cb3aa3728f51234a7"
+Client_ID = "amzn1.application-oa2-client.f19c7c4794b44689aad88c225d3935ae"
+Client_Secret = "b713120c87ec5484745283ce0e00fd2a399df7c48765c52a17cf33e5be4c2745"
 
 # Alexa Refresh Token
-refresh_token = ''
